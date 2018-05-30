b693a9796e3e0814e5f5615a0ac422b3b7e856c0
Abhay Kumar <abhay.kumar@qed42.com>
2018-05-30 21:06:31 +0530

    [TASK]: First commit .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { EmployeeListingComponent } from './components/employee-listing/employee-listing.component';
+import { EmployeeAddComponent } from './components/employee-add/employee-add.component';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+const routes: Routes = [{
+  path: "",
+  component: EmployeeListingComponent
+},
+{
+  path: "employees/add",
+  component: EmployeeAddComponent
+},
+{
+  path: "employees/:data/edit",
+  component: EmployeeEditComponent
+},
+{
+  path: '**',
+  redirectTo: "/",
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeAddComponent } from './components/employee-add/employee-add.component';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+import { EmployeeListingComponent } from './components/employee-listing/employee-listing.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeAddComponent,
+    EmployeeEditComponent,
+    EmployeeListingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
